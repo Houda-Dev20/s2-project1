6d3b58b8874d373ab439b524dcd5d74a607ac256
houda <nounouchil7@gmail.com>
2026-04-17 14:17:27 +0200

    Add wilaya options to signup form
    Populate the wilaya select dropdown with options from a predefined list of wilayas.

--- frontend/scripts/signup.js
+++ frontend/scripts/signup.js
@@ -65,7 +65,27 @@ window.onclick = function(event) {
         }
     }
 };
+const wilayaSelect = document.getElementById("location");
 
+const wilayas = [
+"Adrar","Chlef","Laghouat","Oum El Bouaghi","Batna","Bejaia","Biskra","Bechar",
+"Blida","Bouira","Tamanrasset","Tebessa","Tlemcen","Tiaret","Tizi Ouzou","Algiers",
+"Djelfa","Jijel","Setif","Saida","Skikda","Sidi Bel Abbes","Annaba","Guelma",
+"Constantine","Medea","Mostaganem","Msila","Mascara","Ouargla","Oran","El Bayadh",
+"Illizi","Bordj Bou Arreridj","Boumerdes","El Tarf","Tindouf","Tissemsilt",
+"El Oued","Khenchela","Souk Ahras","Tipaza","Mila","Ain Defla","Naama",
+"Ain Temouchent","Ghardaia","Relizane",
+"Timimoun","Bordj Badji Mokhtar","Ouled Djellal","Beni Abbes",
+"In Salah","In Guezzam","Touggourt","Djanet","El M'Ghair","El Meniaa"
+];
+
+
+wilayas.forEach(w => {
+    let option = document.createElement("option");
+    option.value = w;
+    option.textContent = w;
+    wilayaSelect.appendChild(option);
+});
 //api
 
 const donorForm = document.getElementById("signupForm");
