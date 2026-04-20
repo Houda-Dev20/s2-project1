9a04b598a56be92c339807d311e41ca500ee8135
Houda-Dev20 <nh.ferhane@esi-sba.dz>
2026-04-20 08:46:07 +0100

    Update Privicy_policy.js

--- frontend/scripts/Privicy_policy.js
+++ frontend/scripts/Privicy_policy.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // 1. برمجة زر الرجوع
     const backBtn = document.getElementById('backBtn');
     if (backBtn) {
         backBtn.addEventListener('click', () => {
             if (window.history.length > 1) {
                 window.history.back();
             } else {
-                // إذا لم يكن هناك تاريخ للرجوع، اذهب للرئيسية كخيار بديل
                 window.location.href = 'home.html';
             }
         });
