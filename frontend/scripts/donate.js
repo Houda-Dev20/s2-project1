e51a124163e389762ed2537625d0028984edd2e3
Houda-Dev20 <nh.ferhane@esi-sba.dz>
2026-04-20 08:45:43 +0100

    Update donate.js

--- frontend/scripts/donate.js
+++ frontend/scripts/donate.js
@@ -1,5 +1,4 @@
 function setupFooterHover() {
-    // مصفوفة بأسماء الكلاسات ومسارات الصور (تأكدي من مطابقة الأسماء في مجلد images)
     const socialIcons = [
         { selector: '.sm1-img', hover: 'images/hoverX.svg' },
         { selector: '.sm2-img', hover: 'images/hoverInst.svg' },
@@ -11,12 +10,10 @@ function setupFooterHover() {
         const img = document.querySelector(icon.selector);
         
         if (img) {
-            const originalSrc = img.src; // حفظ الصورة الأصلية
+            const originalSrc = img.src;
 
-            // عند دخول الماوس: تبديل للصورة الملونة
             img.onmouseenter = () => { img.src = icon.hover; };
 
-            // عند خروج الماوس: العودة للصورة الأصلية
             img.onmouseleave = () => { img.src = originalSrc; };
         }
     });
